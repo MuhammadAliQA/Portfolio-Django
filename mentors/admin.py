a238c499fe1b92070d26939f527bf3fe77fa8199
User Userov <user@gmail.com>
2026-03-17 06:20:07 +0500

    fix admin and mobile responsive

--- mentors/admin.py
+++ mentors/admin.py
@@ -10,33 +10,20 @@ class AvailabilityInline(admin.TabularInline):
 
 @admin.register(MentorProfile)
 class MentorProfileAdmin(admin.ModelAdmin):
-    list_display   = ('user', 'primary_track', 'price_per_hour',
-                      'rating', 'students_helped', 'is_featured', 'offers_admission_support')
+    list_display   = ('user', 'primary_track', 'price_per_hour', 'rating',
+                      'students_helped', 'is_featured', 'offers_admission_support')
     list_filter    = ('primary_track', 'is_featured', 'offers_admission_support')
     search_fields  = ('user__username', 'user__first_name', 'user__last_name', 'expertise')
     list_editable  = ('is_featured', 'offers_admission_support', 'price_per_hour')
     ordering       = ('-is_featured', '-rating')
     inlines        = [AvailabilityInline]
-    fieldsets = (
-        ('Asosiy ma\'lumot', {
-            'fields': ('user', 'primary_track', 'expertise', 'headline',
-                       'university_background', 'languages', 'about')
-        }),
-        ('Statistika', {
-            'fields': ('experience_years', 'students_helped',
-                       'success_story_count', 'rating')
-        }),
-        ('Sozlamalar', {
-            'fields': ('price_per_hour', 'is_featured', 'offers_admission_support')
-        }),
-    )
 
 
 @admin.register(MentorAvailability)
 class MentorAvailabilityAdmin(admin.ModelAdmin):
-    list_display  = ('mentor', 'date', 'time', 'is_available')
-    list_filter   = ('is_available', 'date')
-    ordering      = ('date', 'time')
+    list_display = ('mentor', 'date', 'time', 'is_available')
+    list_filter  = ('is_available', 'date')
+    ordering     = ('date', 'time')
 
 
 @admin.register(FavoriteMentor)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
-admin.site.register(User)
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display  = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff', 'is_active', 'date_joined')
+    list_filter   = ('role', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering      = ('-date_joined',)
+    list_editable = ('role', 'is_active')
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('EduBridge', {
+            'fields': ('role', 'bio')
+        }),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('EduBridge', {
+            'fields': ('role', 'bio')
+        }),
+    )
